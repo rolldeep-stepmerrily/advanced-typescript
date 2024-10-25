1f36a9cc9c7c40a8c5c2b9538fa49d7b6d67bdb2
rolldeep-stepmerrily <rolldeep@stepmerrily.com>
2024-10-25 09:56:17 +0900

    refactor : helper

--- src/helper.ts
+++ src/helper.ts
@@ -14,11 +14,9 @@ export type Debug<T> = { [K in keyof T]: T[K] };
 export type MergeInsertions<T> = T extends object ? { [K in keyof T]: MergeInsertions<T[K]> } : T;
 
 export type Alike<X, Y> = Equal<MergeInsertions<X>, MergeInsertions<Y>>;
-
 export type ExpectExtends<VALUE, EXPECTED> = EXPECTED extends VALUE ? true : false;
-export type ExpectValidArgs<FUNC extends (...args: any[]) => any, ARGS extends any[]> = ARGS extends Parameters<FUNC>
-  ? true
-  : false;
+export type ExpectValidArgs<FUNC extends (...args: any[]) => any, ARGS extends any[]> =
+  ARGS extends Parameters<FUNC> ? true : false;
 
 export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void ? I : never;
 
